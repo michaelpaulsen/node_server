0be037a91b7f22a74dc05351f7c89448f09e914b
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-01-01 22:59:14 -0800

    changed how I error handle
    ** put the check to disclude the favicon to above the normal response
    
    ++ return to the if block used to disclude the favicon
    
    -- if around the response ( became redundant after the above changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mime = require('mime-types');
 const hostname = '127.0.0.1';
 const port = 3000;
 const DocRoot = null;
-const favICO = false; 
+const favICO = false;
 
 const server = http.createServer((req, res) => {
 	if(DocRoot == null){
@@ -13,29 +13,29 @@ const server = http.createServer((req, res) => {
 		return;
 		/** give an error if the docroot envorment var is not set*/
 	}
-	if( req.url !== '/favicon.ico' || favICO){
-		var url = req.url;
-		if( url == "/"){
-			url = '/index.html';
-		}
-		fs.readFile(DocRoot + url, function(err, data) {
-			console.log();
-			if(err){
-				res.writeHead(404);
-				res.end();
-				return;
-			}
-			res.writeHead(200, {
-				'Content-Type': mime.lookup(url.split('.')[1])
-				});
-			res.write(data);
-			res.end();
-		});
+	if( url == "/"){
+		url = '/index.html';
 	}
 	if( req.url == '/favicon.ico' && !favICO){
 		res.writeHead(200);
 		res.end();
+		return;
 	}
+	var url = req.url;
+	
+	fs.readFile(DocRoot + url, function(err, data) {
+		console.log();
+		if(err){
+			res.writeHead(404);
+			res.end();
+			return;
+		}
+		res.writeHead(200, {
+			'Content-Type': mime.lookup(url.split('.')[1])
+			});
+		res.write(data);
+		res.end();
+	});	
 });
 
 server.listen(port, hostname, () => {
